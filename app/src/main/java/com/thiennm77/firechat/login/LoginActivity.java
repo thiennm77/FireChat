f8d43d2056ea3495f61fb57a029fc873f2a2b3cc
thiennm77 <thiennm77@gmail.com>
2017-01-21 13:11:47 +0700

    layout for register screen

---
+++ app/src/main/java/com/thiennm77/firechat/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.thiennm77.firechat.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.thiennm77.firechat.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/thiennm77/firechat/register/RegisterActivity.java
+++ app/src/main/java/com/thiennm77/firechat/register/RegisterActivity.java
@@ -1,13 +1,15 @@
-package com.thiennm77.firechat;
+package com.thiennm77.firechat.register;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class LoginActivity extends Activity {
+import com.thiennm77.firechat.R;
+
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 }
